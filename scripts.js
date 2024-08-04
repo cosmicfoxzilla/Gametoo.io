029d89925b396298528398c9ef872b8fb88333ce
DevMikey123 <160168165+DevMikey123@users.noreply.github.com>
2024-08-04 16:44:41 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+$(document).ready(function() {
+    // Dark mode toggle
+    const darkModeToggle = document.createElement('button');
+    darkModeToggle.classList.add('dark-mode-toggle');
+    darkModeToggle.innerHTML = '<span class="material-icons">dark_mode</span>';
+    document.body.appendChild(darkModeToggle);
 
+    darkModeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        darkModeToggle.innerHTML = document.body.classList.contains('dark-mode')
+            ? '<span class="material-icons">light_mode</span>'
+            : '<span class="material-icons">dark_mode</span>';
+    });
+
+    // Search functionality
+    $('#search-input').on('keyup', function() {
+        var value = $(this).val().toLowerCase();
+        $('#content .item').filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+</script>
