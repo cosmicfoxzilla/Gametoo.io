f13d84eac3db10ccb62f0d8d450ad2603463755b
DevMikey123 <160168165+DevMikey123@users.noreply.github.com>
2024-08-04 17:13:32 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,6 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
 $(document).ready(function() {
     // Dark mode toggle
-    const darkModeToggle = document.createElement('button');
-    darkModeToggle.classList.add('dark-mode-toggle');
-    darkModeToggle.innerHTML = '<span class="material-icons">dark_mode</span>';
-    document.body.appendChild(darkModeToggle);
-
+    const darkModeToggle = document.querySelector('.dark-mode-toggle');
     darkModeToggle.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
         darkModeToggle.innerHTML = document.body.classList.contains('dark-mode')
@@ -17,9 +11,8 @@ $(document).ready(function() {
     // Search functionality
     $('#search-input').on('keyup', function() {
         var value = $(this).val().toLowerCase();
-        $('#content .item').filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        $('.game-item').filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
         });
     });
 });
-</script>
