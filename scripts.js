48098403c0c6178e307c7d2c778565bd756ac360
DevMikey123 <160168165+DevMikey123@users.noreply.github.com>
2024-08-04 17:28:10 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
     // Dark mode toggle
     const darkModeToggle = document.querySelector('.dark-mode-toggle');
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+    if (prefersDarkScheme.matches) {
+        document.body.classList.add('dark-mode');
+        darkModeToggle.innerHTML = '<span class="material-icons">light_mode</span>';
+    }
+
     darkModeToggle.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
         darkModeToggle.innerHTML = document.body.classList.contains('dark-mode')
@@ -9,6 +16,18 @@ $(document).ready(function() {
     });
 
     // Search functionality
+    $('.search-icon').on('click', function() {
+        $('#search-input').toggle();
+        $('.clear-icon').toggle();
+        $('#search-input').focus();
+    });
+
+    $('.clear-icon').on('click', function() {
+        $('#search-input').val('').hide();
+        $('.clear-icon').hide();
+        $('.game-item').show();
+    });
+
     $('#search-input').on('keyup', function() {
         var value = $(this).val().toLowerCase();
         $('.game-item').filter(function() {
